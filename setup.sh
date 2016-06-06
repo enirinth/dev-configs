a57a380dd991de90f0a76696a4bbe5a2167d789d
Aaron <enirinth@umich.edu>
2016-06-06 11:20:31 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 cp -i .vimrc ~/
 
 cp -i onedark.vim ~/.vim/colors/ # vim one-dark editor color theme
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # pathogen - vim plugin management
+
+# install pathogen - vim plugin management
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
 echo 'execute pathogen#infect()' >> ~/.vimrc 
 echo 'filetype plugin indent on' >> ~/.vimrc
