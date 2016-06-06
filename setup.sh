c8e95ae9e5614b9bac35a72fec299cd321a7a97c
enirinth <enirinth@umich.edu>
2016-06-05 23:59:24 -0700

    added Nerdtree

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 # Only run this script if this is a new dev-vm
-# It basically just copies config files to corresponding path
 
 cp -i .vimrc ~/
-cp -i onedark.vim ~/.vim/colors/
+
+cp -i onedark.vim ~/.vim/colors/ # vim one-dark editor color theme
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # pathogen - vim plugin management
+echo 'execute pathogen#infect()' >> ~/.vimrc 
+echo 'filetype plugin indent on' >> ~/.vimrc
