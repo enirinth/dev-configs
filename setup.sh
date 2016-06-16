04adae801e3b89a117701031c2b86dc48b4ca158
Haoran Wang <haoran.wang@nutanix.com>
2016-06-15 17:28:05 -0700

    not echoing in setup.sh, copying .vimrc as a whole instead

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 # Only run this script if this is a new dev-vm
+# There are functions needed to be added to vimrc, without echoing here, just copy the whole vimrc file
 
 cp -i .vimrc ~/
 
 # vim one-dark editor color theme
 mkdir -p ~/.vim/colors/
-cp -i onedark.vim ~/.vim/colors/ 
+cp -i onedark.vim ~/.vim/colors/
+
+# vim protobuf syntax highlighting
+mkdir -p ~/.vim/syntax/
+cp -i proto.vim ~/.vim/syntax/ 
 
 # install pathogen - vim plugin management
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
-echo 'execute pathogen#infect()' >> ~/.vimrc 
-echo 'filetype plugin indent on' >> ~/.vimrc
-
-# command alias for NERDTree
-echo ':command NT NERDTree' >> ~/.vimrc
