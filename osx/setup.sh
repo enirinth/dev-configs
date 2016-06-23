661528adbbf7b85d4f81058219b4a7b833f7bebb
Aaron <enirinth@umich.edu>
2016-06-23 13:36:13 -0700

    Update setup.sh

--- osx/setup.sh
+++ osx/setup.sh
@@ -11,9 +11,10 @@ cp -i onedark.vim ~/.vim/colors/
 mkdir -p ~/.vim/syntax/
 cp -i proto.vim ~/.vim/syntax/ 
 
-# install pathogen - vim plugin management
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
-
 # create symlink from built-in vim to MacVim
 echo '# create symlink from MacVim to built-in vim' >> ~/.bashrc
 echo "alias vim='/usr/local/Cellar/macvim/7.4-104/MacVim.app/Contents/MacOS/Vim'" >> ~/.bashrc
+
+# YCM semantic-autocomplete bundle
+mkdir -p ~/.vim/bundle/YouCompleteMe/cpp
+cp -i .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/cpp
